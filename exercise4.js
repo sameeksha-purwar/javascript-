379f3350f27b54e16e2f8ea7996b6c7e27e43947
Sameeksha purwar <69745044+sameeksha-purwar@users.noreply.github.com>
2023-02-14 12:06:14 +0530

    Add files via upload

---
+++ exercise4.js
@@ -0,0 +1,10 @@
+function left(age){
+    var x=90-age
+    var y= x*365
+    var z= x*52
+    var a=x*12
+
+    console.log("you have"+" "+ y+" " +"days," +" "+z+" "+ "weeks and"+" "+ a+" " +"months left")
+}
+
+left(50)
