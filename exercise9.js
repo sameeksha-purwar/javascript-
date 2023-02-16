a3fe61eb09e5e26e577b18237efce523b7e0dabc
Sameeksha purwar <69745044+sameeksha-purwar@users.noreply.github.com>
2023-02-16 21:53:37 +0530

    Add files via upload

---
+++ exercise9.js
@@ -0,0 +1,45 @@
+// arr=[5,6,36]
+
+// let s1=arr.filter((value,ind,array)=>{
+    
+//     // if( value%2==0){
+//     //     return value
+//     // }
+//     for(var i=0;i<array.length;i++){
+//         if(array[i]%2==0){
+//             return value
+//         }
+//     }
+    
+// })
+// console.log(s1)
+// let age=prompt("Enter age")
+
+// let s= age=>{
+//     if (age<18){
+//         alert("you are not eligible")
+//     }
+//     else{
+//         alert("you are eliginle")
+//     }
+
+// }
+// console.log(s(age))
+
+//arr=[1,2,3,4,5,6]
+// console.log(arr.reduce((acc,value)=>{
+//     return acc+value
+// }))
+//let sum=0
+// for(var i=0;i<arr.length;i++){
+//     sum=sum+arr[i]
+// }
+// console.log(sum)
+//console.log(reverse(arr.begin(),arr.end()))
+// for(var i=arr.length-1;i>=0;i--){
+//     console.log(arr[i])
+// }
+var a=3
+var b=a++
+var c=++a
+console.log(a,b,c)
